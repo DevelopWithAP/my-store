487022e51e6d2f5ccfaaa8de80277cc33dd011fd
Viktorija Bartasiute <viktorijabartasiute@Viktorijas-Air.home>
2022-03-13 19:38:14 +0000

    Deleted unused method

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Customer } from '../../../Customer';
-import { checkCard } from '../../../checkCard';
 
 @Component({
   selector: 'app-customer',
--- src/checkCard.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl } from '@angular/forms';
-
-export const checkCard = (control: AbstractControl) => {
-    if (control.value.length === 16) {
-        return { invalid: false }
-    }
-    return null;
-};
